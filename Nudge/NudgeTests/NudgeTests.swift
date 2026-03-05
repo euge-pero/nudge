740c5c209d6411841782951f7f8e4492f2d4f205
Maria Perotti <maria.perotti@measured.com>
2026-03-05 17:14:41 -0300

    Initial commit

---
+++ Nudge/NudgeTests/NudgeTests.swift
@@ -0,0 +1,16 @@
+//
+//  NudgeTests.swift
+//  NudgeTests
+//
+//  Created by Maria Eugenia Perotti on 05/03/2026.
+//
+
+import Testing
+
+struct NudgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
